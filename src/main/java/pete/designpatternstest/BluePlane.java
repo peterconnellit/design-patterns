d60eab36ea7177edf5723f386e38a0edf33e825e
Peter Connell <peterconnellit@gmail.com>
2023-04-06 19:07:06 +0100

    // Template Pattern feat:template pattern defers algorithm steps to subclass  ; Sprite to BluePlane

---
+++ src/main/java/pete/designpatternstest/BluePlane.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pete.designpatternstest;
+
+import java.awt.image.BufferedImage;
+
+/**
+ *
+ * @author peter
+ */
+
+public class BluePlane extends Sprite{
+    public BluePlane (int x, int y, BufferedImage image){
+        super(x, y, image);
+    }
+    
+}
