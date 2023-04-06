52e5b05aae98977b0d76c83f3a75104f115b7744
Peter Connell <peterconnellit@gmail.com>
2023-04-06 19:06:00 +0100

    // Template Pattern feat:template pattern defers algorithm steps to subclass  ; Sprite to BluePlane

---
+++ src/main/java/pete/designpatternstest/DesignPatternsTest.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package pete.designpatternstest;
+
+import java.awt.BorderLayout;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author peter
+ */
+
+public class DesignPatternsTest {
+
+    public static void main(String[] args) {
+        
+        JFrame frame = new JFrame("Design Pattern Template");
+        Canvas canvas = new Canvas();
+        frame.add(canvas, BorderLayout.CENTER);
+        frame.setSize(300,350);
+        canvas.setFocusable(true);
+        frame.setVisible(true);
+        canvas.requestFocus();
+    }
+}
